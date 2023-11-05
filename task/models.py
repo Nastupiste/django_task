95a8964c071a2e34a99914f64761e2cd1c1282f4
Nastupiste <mjroscos@gmail.com>
2023-11-06 00:23:59 +0100

    modifcaciones en models.py

--- task/models.py
+++ task/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 
-class task(models.Model):
+class Task(models.Model):
     title = models.CharField(max_length=150)
-    description = models.TextField()
+    description = models.TextField(max_length=200)
     completed = models.BooleanField(default=False)
 
     def __str__(self):
--- task/views.py
+++ task/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from .models import task
+from .models import Task
 # Create your views here.
 
 
 def task_list(request):
-    tasks = task.objects.all()
+    tasks = Task.objects.all()
     return render(request, 'task_list.html', {'tasks': tasks})
